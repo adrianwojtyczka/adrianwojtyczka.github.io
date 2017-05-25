1888169c8496e7e050706686ea8e4e7ce652db77
adrian.wojtyczka <adrian.wojtyczka@hotmail.com>
2017-05-25 08:04:26 -0400

    Updated frontend languages icons. Fixed smooth scroll and scroll spy. Small text fixes. Added Google Analytics.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,33 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
     
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({target: '.navbar-fixed-top'})
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
     
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
-      $(".navbar-collapse").collapse('hide');});
+        $(".navbar-collapse").collapse('hide');
+    });
     
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
 
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
+            
             // Prevent default anchor click behavior
-        event.preventDefault();
+            event.preventDefault();
 
-        // Store hash
-        var hash = this.hash;
+            // Store hash
+            var sender = this;
+            var hash = this.hash;
 
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 1000, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 50
+              }, 1000, function() {
+
+                // Reset focus state
+                $(sender).blur();
+                
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                //window.location.hash = hash;
+            });
+        } // End if
+    });
 });
   
